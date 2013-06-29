86f0969f5b764bbc0b0d3225234e98b5c751eb7b
Rob Bell <me@rob-bell.net>
2013-06-29 13:08:43 +0700

    Added additional test case to OffsetSampleExpressionTest

--- DrumScore.Tests/Expressions/OffsetSampleExpressionTest.cs
+++ DrumScore.Tests/Expressions/OffsetSampleExpressionTest.cs
@@ -8,7 +8,7 @@ namespace DrumScore.Tests.Expressions
     public class OffsetSampleExpressionTest
     {
         [Test]
-        public void EarlyExpressionMovesScorePosition()
+        public void EarlyExpressionMovesScorePositionBackwards()
         {
             const int offset = 2;
             var score = new Mock<IScore>();
@@ -17,6 +17,20 @@ namespace DrumScore.Tests.Expressions
             expression.Interpret(score.Object);
 
             score.Verify(s => s.SetPosition(-offset));
+            score.Verify(s => s.AddSample(It.IsAny<Sample>()), Times.Once());
+            score.Verify(s => s.Progress());
+        }
+
+        [Test]
+        public void LateExpressionMovesScorePositionForwards()
+        {
+            const int offset = 4;
+            var score = new Mock<IScore>();
+            var expression = new OffsetSampleExpression("=") { Offset = offset, Timing = OffsetTiming.Late };
+
+            expression.Interpret(score.Object);
+
+            score.Verify(s => s.SetPosition(offset));
             score.Verify(s => s.AddSample(It.IsAny<Sample>()), Times.Once());
             score.Verify(s => s.Progress());
         }

0cc140d2b59a6097670508a4789de78294981ea3
Rob Bell <me@rob-bell.net>
2013-09-25 14:52:59 +0700

    Added OscOutput implementation of IPlaybackOutput

---
+++ DrumScore/OscOutput.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using Bespoke.Common.Osc;
+
+namespace DrumScore
+{
+    public class OscOutput : IPlaybackOutput
+    {
+        private IPEndPoint endPoint = new IPEndPoint(IPAddress.Loopback, 12013);
+
+        public void Play(ICollection<Sample> samples)
+        {
+            var message = new OscMessage(endPoint, "/");
+
+            foreach (var sample in samples)
+            {
+                message.Append(sample.Type);
+            }
+
+            message.Send(endPoint);
+        }
+    }
+}

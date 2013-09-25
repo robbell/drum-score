036ed9e0fb2d49dc14889194e34e616f7f1d4ef5
Rob Bell <me@rob-bell.net>
2013-09-25 14:41:08 +0700

    Added DummyReceiver console application

---
+++ DrumScore.DummyReceiver/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using Bespoke.Common.Osc;
+
+namespace DrumScore.DummyReceiver
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var server = new OscServer(TransportType.Udp, IPAddress.Loopback, 12013);
+            server.MessageReceived += MessageReceived;
+            server.Start();
+
+            Console.WriteLine("DummyReceiver Started");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+            server.Stop();
+        }
+
+        private static void MessageReceived(object sender, OscMessageReceivedEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}

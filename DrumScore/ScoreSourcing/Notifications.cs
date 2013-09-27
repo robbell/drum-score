b958c69664556140b20b26ad3ac0f74608b19da0
Rob Bell <me@rob-bell.net>
2013-09-27 17:24:43 +0700

    Adding missing Notifications class

---
+++ DrumScore/ScoreSourcing/Notifications.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using DrumScore.Interpretation;
+using Tweetinvi;
+
+namespace DrumScore.ScoreSourcing
+{
+    public class Notifications : INotifications
+    {
+        public void SendError(ScoreInfo invalidScore, UnrecognisedTokenException exception)
+        {
+            try
+            {
+                var user = new TokenUser(TokenSingleton.Token);
+
+                var tweet = string.Format("@{0} Unrecognised token: \"{1}\". Full details: {2}",
+                                          invalidScore.Username,
+                                          exception.Message,
+                                          exception)
+                                  .Substring(0, 140);
+
+                user.SendTweet(tweet);
+            }
+            catch (WebException)
+            {
+                // ToDo: implement UI notifications
+            }
+        }
+    }
+}

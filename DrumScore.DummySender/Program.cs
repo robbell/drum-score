ab6d40a645c29ded7e8c6a2d3827e010ff1d7ead
Rob Bell <me@rob-bell.net>
2013-11-24 15:52:22 +0700

    Added DummySender project

---
+++ DrumScore.DummySender/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using Bespoke.Common.Osc;
+
+namespace DrumScore.DummySender
+{
+    public class Program
+    {
+        private static readonly IPEndPoint endPoint = new IPEndPoint(IPAddress.Loopback, 12015);
+
+        public static void Main()
+        {
+            Console.WriteLine("Dummy Sender started.");
+
+            while (true)
+            {
+                Console.WriteLine("Type message body and press enter:");
+                SendMessage(Console.ReadLine());
+
+                Console.WriteLine("Message sent.");
+                Console.WriteLine();
+            }
+        }
+
+        private static void SendMessage(string messageText)
+        {
+            var message = new OscMessage(endPoint, "/");
+
+            message.Append(messageText);
+
+            message.Send(endPoint);
+        }
+    }
+}
